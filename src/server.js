c18fba03fb0ff4b229fda5b2f421a76211bcfc33
bribsz <brenohribeiroh@gmail.com>
2025-11-30 07:00:12 -0300

    alterando o server.js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,4 @@ const routes = require("./routes/orderRoutes");
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("API rodando na porta 3000");
-});
+app.listen(3000, () => console.log("API rodando na porta 3000"));
