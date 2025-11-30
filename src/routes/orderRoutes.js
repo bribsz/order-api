2ad8b1b125389e45cb2817ef98f6e6db26ff991a
bribsz <brenohribeiroh@gmail.com>
2025-11-30 16:10:13 -0300

    Corrigindo a chamada pro endpoint order/list

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -5,9 +5,10 @@ const auth = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/order", auth, controller.create);
-router.get("/order/:id", auth, controller.get);
 router.get("/order/list", auth, controller.list);
+router.get("/order/:id", auth, controller.get);
 router.put("/order/:id", auth, controller.update);
 router.delete("/order/:id", auth, controller.remove);
+
 
 module.exports = router;
